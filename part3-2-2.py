1800fe40205a310924fda3815355f2bfa153b0f4
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2022-02-10 00:20:41 +0300

    Add files via upload
    image processing course

--- part3-2-2.py
+++ part3-2-2.py
@@ -6,6 +6,30 @@ from skimage.util import img_as_float, img_as_ubyte
 img = imread(r'3.2\img.png')
 img_f = skimage.img_as_float(img)
 
-R_averaged = img_f[:,:,0] // img_f[:,:,0].size
-G_averaged = img_f[:,:,1]
-B_averaged = img_f[:,:,2]
+R_averaged = img_f[:,:,0].ravel().mean()
+G_averaged = img_f[:,:,1].ravel().mean()
+B_averaged = img_f[:,:,2].ravel().mean()
+
+RGB_averaged = R_averaged + G_averaged + B_averaged
+AVG = RGB_averaged / 3
+
+r_w = R_averaged / AVG
+g_w = G_averaged / AVG
+b_w = B_averaged / AVG
+
+img_f = skimage.img_as_float(img)
+R = img_f[:,:,0] / r_w
+G = img_f[:,:,1] / g_w
+B = img_f[:,:,2] / b_w
+
+R = np.clip(R, 0, 1.0)
+G = np.clip(G, 0, 1.0)
+B = np.clip(B, 0, 1.0)
+
+R = img_as_ubyte(R)
+G = img_as_ubyte(G)
+B = img_as_ubyte(B)
+rgb = np.dstack((R,G,B))
+imsave(r'3.2\out_img.png', rgb)
+
+print(np.array_equal(rgb, imread(r'3.2\railroad-gray-world.png')))
