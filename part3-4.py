324a8c7e145dad7e1a65edfd44132f49b5c934e4
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2022-02-10 21:20:30 +0300

    Update part3-4.py
    image processing course

--- part3-4.py
+++ part3-4.py
@@ -3,16 +3,24 @@ import skimage
 from skimage.io import imread, imsave
 from skimage.util import img_as_float, img_as_ubyte
 from numpy import histogram
+import datetime
 
-img = imread(r'landscape.png')
+print(datetime.datetime.now())
+
+img = imread('img.png')
+
+def calculate_fx(x):
+    
+    return round(((np.sum(values[0:x+1]) - np.min(np.nonzero(np.sum(values[0:x+1])))) / (img.size - 1)) * 255)
+
 values, bin_edges = histogram(img.ravel(), bins=range(257))
-
-cdf = 0
-for i in range(values.size):
-    cdf = cdf + values[i]
+cdf_min = np.min(values[np.nonzero(values)])
 
 for i in range(img.shape[0]):
     for j in range(img.shape[1]):
-        img[i][j] = 
+        img[i,j] = calculate_fx(img[i,j])
 
-print("")
+imsave("out_img.png", img)
+print(np.array_equal(img, imread('landscape-histeq.png')))
+
+print(datetime.datetime.now())
