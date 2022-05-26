a7ae00ff6377261f46219e185272ce22ffae20a1
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2022-05-26 23:20:07 +0200

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -8,11 +8,12 @@ def show(img):
     plt.imshow(img)
     plt.show()
 
-#step 1
+# step 1
+# Подготовьте одно изображение для экспериментов с гауссовской и лапласовской пирамидой.
 apple = imread("apple.bmp")
 orange = imread("orange.bmp")
 
-#step 2
+#step 2.1
 #Постройте гауссовскую пирамиду изображения из не менее чем пяти слоев
 g1 = skimage.filters.gaussian(apple, sigma=2, mode='nearest')
 g2 = skimage.filters.gaussian(g1, sigma=2, mode='nearest')
@@ -20,6 +21,36 @@ g3 = skimage.filters.gaussian(g2, sigma=3, mode='nearest')
 g4 = skimage.filters.gaussian(g3, sigma=3, mode='nearest')
 g5 = skimage.filters.gaussian(g4, sigma=4, mode='nearest')
 
-freq = np.log(1 + abs(np.fft.fftshift(np.fft.fft2(g1))))
+# step 2.2
+# Визуализируйте полученные изображения и амплитуды частот изображений пирамиды
+freq1 = np.log(1 + abs(np.fft.fftshift(np.fft.fft2(g1))))
+freq2 = np.log(1 + abs(np.fft.fftshift(np.fft.fft2(g2))))
+freq3 = np.log(1 + abs(np.fft.fftshift(np.fft.fft2(g3))))
+freq4 = np.log(1 + abs(np.fft.fftshift(np.fft.fft2(g4))))
+freq5 = np.log(1 + abs(np.fft.fftshift(np.fft.fft2(g5))))
 
-show(freq[:,:,0])
+fig = plt.figure(figsize=(18, 8))
+columns = 3
+rows = 2
+fig.add_subplot(rows, columns, 1)
+plt.imshow(freq1[:,:,0])
+fig.add_subplot(rows, columns, 2)
+plt.imshow(freq2[:,:,0])
+fig.add_subplot(rows, columns, 3)
+plt.imshow(freq3[:,:,0])
+fig.add_subplot(rows, columns, 4)
+plt.imshow(freq4[:,:,0])
+fig.add_subplot(rows, columns, 5)
+plt.imshow(freq5[:,:,0])
+plt.show()
+
+# и убедитесь, что на каждом слое диапазон частот сужается
+#print(np.mean(freq1))
+#print(np.mean(freq2))
+#print(np.mean(freq3))
+#print(np.mean(freq4))
+#print(np.mean(freq5))
+print(np.mean(freq1) > np.mean(freq2) and np.mean(freq2) > np.mean(freq3) and np.mean(freq3) > np.mean(freq4) and np.mean(freq4) > np.mean(freq5))
+
+# step 2.3
+# Постройте пирамиду для трех различных значения сигмы гауссовского ядра
