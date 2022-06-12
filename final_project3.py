b9d81da00dfd1c75679c3fa2fda621ec89446f1d
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2022-06-12 23:43:36 +0200

    Update final_project3.py
    homework blending images

--- final_project3.py
+++ final_project3.py
@@ -8,39 +8,59 @@ def show(img):
     plt.imshow(img)
     plt.show()
 
+def extend_image(img, kernel_width):
+
+    #extend image if kernel equals 7*7
+    #it means that we have to extend each border to 3 pixels
+    #and corners
+
+    #the mirror method
+    #get radius
+    r = kernel_width // 2
+
+    #make new extended image
+    new_img = np.zeros((img.shape[0] + (r * 2), img.shape[1] + (r * 2), img.shape[2]) ,np.uint8)
+
+    #top
+    top = img[0:r, 0:img.shape[1]]
+    top = top[::-1]
+    new_img[0:r, r:img.shape[1] + r] = top
+    #corners on the top
+    new_img[0:r, 0:r] = top[0:r, 0:r]
+    new_img[0:r, new_img.shape[1] - r: new_img.shape[1]] = img[0:r, img.shape[1] - r: img.shape[1]]
+
+    #bottom
+    bottom = img[img.shape[0] - r:img.shape[0], 0:img.shape[1]]
+    bottom = bottom[::-1]
+    new_img[new_img.shape[0] - r:new_img.shape[0], r:img.shape[1] + r] = bottom
+    #corners on the bottom
+    new_img[new_img.shape[0] - r:new_img.shape[0], 0:r] = bottom[bottom.shape[0] - r:new_img.shape[0], 0:r]
+    new_img[new_img.shape[0] - r:new_img.shape[0], new_img.shape[1] - r:new_img.shape[1]] = bottom[bottom.shape[0] - r:new_img.shape[0], img.shape[1] - r:img.shape[1]]
+
+    #left
+    left = img[0:img.shape[0], 0:r]
+    left = np.flip(left, axis=1)
+    new_img[r:img.shape[0] + r, 0:r] = left
+
+    #right
+    right = img[0:img.shape[0], img.shape[1] - r:img.shape[1]]
+    right = np.flip(right, axis=1)
+    new_img[r:img.shape[0] + r, new_img.shape[1] - r:new_img.shape[1]] = right
+
+    #center
+    new_img[r: new_img.shape[0] - r, r: new_img.shape[1] - r] = img
+
+    return new_img
+
+def apply_gauss_blur(img, sigma, n_layers):
+
+    gauss_images = []
+    
+
 # step 1
 # Подготовьте одно изображение для экспериментов с гауссовской и лапласовской пирамидой.
 apple = imread("apple.bmp")
 orange = imread("orange.bmp")
 
-#step 2.1
-#Постройте гауссовскую пирамиду изображения из не менее чем пяти слоев
-g1 = skimage.filters.gaussian(apple, sigma=1, mode='nearest')
-g2 = skimage.filters.gaussian(g1, sigma=3, mode='nearest')
-g3 = skimage.filters.gaussian(g2, sigma=5, mode='nearest')
-g4 = skimage.filters.gaussian(g3, sigma=7, mode='nearest')
-g5 = skimage.filters.gaussian(g4, sigma=9, mode='nearest')
-
-# step 2.3
 
 
-fig = plt.figure(figsize=(16, 8), dpi=100)
-columns = 3
-rows = 2
-
-fig.add_subplot(rows, columns, 1)
-plt.imshow(apple)
-fig.add_subplot(rows, columns, 2)
-plt.imshow(g1)
-fig.add_subplot(rows, columns, 3)
-plt.imshow(g2)
-fig.add_subplot(rows, columns, 4)
-plt.imshow(g3)
-fig.add_subplot(rows, columns, 5)
-plt.imshow(g4)
-fig.add_subplot(rows, columns, 6)
-plt.imshow(g5)
-
-plt.show()
-print("")
-plt.clf()
