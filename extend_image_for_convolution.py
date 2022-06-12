2bd61ae16b00c1df2b936de395935a3bc03bc1f0
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2022-06-12 16:15:53 +0200

    Add files via upload

--- extend_image_for_convolution.py
+++ extend_image_for_convolution.py
@@ -12,7 +12,18 @@ print(img.shape)
 
 #extend image if kernel equals 7*7
 #it means that we have to extend each border to 3 pixels
+w = 7
 
 #the mirror method
+#get radius
+r = w // 2
 
+#make new extended image
+new_img = np.zeros((img.shape[0] + (r * 2), img.shape[1] + (r * 2), img.shape[2]) ,np.uint8)
+print(new_img.shape)
 
+#top
+top = img[0:r, 0:img.shape[1]]
+print(top.shape)
+#bottom
+bottom = img[0:r, 0:img.shape[1]]
