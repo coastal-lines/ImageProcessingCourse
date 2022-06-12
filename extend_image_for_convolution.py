89d068857e4cc86a27b08cc84a54ff868f81bdcb
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2022-06-12 23:37:04 +0200

    Update extend_image_for_convolution.py
    extend image for gauss

--- extend_image_for_convolution.py
+++ extend_image_for_convolution.py
@@ -7,31 +7,51 @@ def show(img):
     plt.imshow(img)
     plt.show()
 
-img = imread("apple.bmp")
-print(img.shape)
+img = imread("apple2.bmp")
 
-#extend image if kernel equals 7*7
-#it means that we have to extend each border to 3 pixels
-w = 7
+def extend_image(img, kernel_width):
 
-#the mirror method
-#get radius
-r = w // 2
+    #extend image if kernel equals 7*7
+    #it means that we have to extend each border to 3 pixels
+    #and corners
 
-#make new extended image
-new_img = np.zeros((img.shape[0] + (r * 2), img.shape[1] + (r * 2), img.shape[2]) ,np.uint8)
-print(new_img.shape)
+    #the mirror method
+    #get radius
+    r = kernel_width // 2
 
-print("===")
-#top
-top = img[0:r, 0:img.shape[1]]
-print(top.shape)
-#bottom
-bottom = img[img.shape[0]-r:img.shape[0], 0:img.shape[1]]
-print(bottom.shape)
-#left
-left = img[0:img.shape[0], 0:r]
-print(left.shape)
-#right
-right = img[0:img.shape[0], img.shape[1] - r:img.shape[1]]
-print(right.shape)
+    #make new extended image
+    new_img = np.zeros((img.shape[0] + (r * 2), img.shape[1] + (r * 2), img.shape[2]) ,np.uint8)
+
+    #top
+    top = img[0:r, 0:img.shape[1]]
+    top = top[::-1]
+    new_img[0:r, r:img.shape[1] + r] = top
+    #corners on the top
+    new_img[0:r, 0:r] = top[0:r, 0:r]
+    new_img[0:r, new_img.shape[1] - r: new_img.shape[1]] = img[0:r, img.shape[1] - r: img.shape[1]]
+
+    #bottom
+    bottom = img[img.shape[0] - r:img.shape[0], 0:img.shape[1]]
+    bottom = bottom[::-1]
+    new_img[new_img.shape[0] - r:new_img.shape[0], r:img.shape[1] + r] = bottom
+    #corners on the bottom
+    new_img[new_img.shape[0] - r:new_img.shape[0], 0:r] = bottom[bottom.shape[0] - r:new_img.shape[0], 0:r]
+    new_img[new_img.shape[0] - r:new_img.shape[0], new_img.shape[1] - r:new_img.shape[1]] = bottom[bottom.shape[0] - r:new_img.shape[0], img.shape[1] - r:img.shape[1]]
+
+    #left
+    left = img[0:img.shape[0], 0:r]
+    left = np.flip(left, axis=1)
+    new_img[r:img.shape[0] + r, 0:r] = left
+
+    #right
+    right = img[0:img.shape[0], img.shape[1] - r:img.shape[1]]
+    right = np.flip(right, axis=1)
+    new_img[r:img.shape[0] + r, new_img.shape[1] - r:new_img.shape[1]] = right
+
+    #center
+    new_img[r: new_img.shape[0] - r, r: new_img.shape[1] - r] = img
+
+    return new_img
+
+extended_img = extend_image(img, 7)
+show(extended_img)
